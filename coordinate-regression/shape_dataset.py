2e531668f75726b6d687a79b4ad4ca383dc3141b
Jens E. Pedersen <jensegholm@protonmail.com>
2022-11-16 15:56:11 +0100

    Used sparse files in dataset

--- coordinate-regression/shape_dataset.py
+++ coordinate-regression/shape_dataset.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import torch
 
-
 class ShapeDataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -11,7 +10,8 @@ class ShapeDataset(torch.utils.data.Dataset):
         train: bool = True,
         pose_offset: torch.Tensor = torch.Tensor([0, 0]),
         pose_delay: int = 0,
-        frames_per_file: int = 128
+        frames_per_file: int = 128,
+        device: str = "cpu",
     ) -> None:
         super().__init__()
         self.files = list(Path(root).glob("*.dat"))
@@ -24,20 +24,22 @@ class ShapeDataset(torch.utils.data.Dataset):
         self.pose_delay = int(pose_delay)
         self.chunks = frames_per_file // (2 * t + self.pose_delay)
         self.pose_offset = pose_offset
+        self.device = device
         assert len(self.files) > 0, f"No data files in given root '{root}'"
 
     def __getitem__(self, index):
-        _, frames, poses, _ = torch.load(self.files[index // self.chunks])
+        filename = self.files[index // self.chunks]
+        frames, poses = torch.load(filename, map_location=self.device)
+        frames = frames.to_dense()
         chunk = index % self.chunks
         start = chunk * self.t
         mid = start + self.t
         end = mid + self.t
-        warmup_tensor = frames[start: mid].float()
-        actual_tensor = frames[mid: end].float()
-        delayed_poses = poses[mid + self.pose_delay: end + self.pose_delay]
+        warmup_tensor = frames[start:mid].float()
+        actual_tensor = frames[mid:end].float()
+        delayed_poses = poses[mid + self.pose_delay : end + self.pose_delay]
         offset_poses = delayed_poses + self.pose_offset
         return warmup_tensor, actual_tensor, offset_poses.squeeze()
 
     def __len__(self):
         return len(self.files) * self.chunks
-
